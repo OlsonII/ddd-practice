4c641d79529151a968ee853802060f76b495bb9e
Nilson Rosado <30422101+OlsonII@users.noreply.github.com>
2020-10-16 15:35:43 -0500

    Ajustes menores
    v1.0.0

--- src/infrastructure/infrastructure.module.ts
+++ src/infrastructure/infrastructure.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { BankAccountRepository } from './repositories/bankAccount.repository';
-import { FinancialMovementRepository } from './repositories/financialMovement.repository';
 import { UnitOfWork } from './unitOfWork/unitOfWork';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [BankAccountRepository, FinancialMovementRepository, UnitOfWork],
-  exports: [BankAccountRepository, FinancialMovementRepository, UnitOfWork]
+  providers: [UnitOfWork],
+  exports: [UnitOfWork]
 })
 export class InfrastructureModule{}
